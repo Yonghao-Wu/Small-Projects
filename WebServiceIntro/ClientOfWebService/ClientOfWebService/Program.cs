7383d370493e5ac199d82c35aa0d4b5019cf1a23
dizchynzboi <markwong56@yahoo.com>
2017-09-03 00:15:40 -0700

    First Web Service

---
+++ WebServiceIntro/ClientOfWebService/ClientOfWebService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientOfWebService
+{
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// 4. I added a "web reference" to the project, the web reference is the URL of the web service, in this case: http://localhost:57125/MathService.asmx
+        ///         Add a service reference, go to advanced, then "Add web reference", type or paste the URL, then Add Reference. 
+        /// 5. Web reference name is simply the object name you give to the web service, as a shortcut, in this case: localhost1
+        /// 6. Afterwards, you create a variable of the web service object, then you choose the class in that web service, and finally the method. 
+        ///         In this case, the class is the WebService1, and the method is .Add within that class. There are three other classes
+        ///         Subtract, Multiply, and Divide, you can change it up. 
+        /// 7. Then display the results in console. 
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            localhost1.WebService1 myMathWebService = new localhost1.WebService1();
+            //Change up the methods if wanted. 
+            Console.Write("Add {0} & {1} = " + myMathWebService.Add(2, 4), 2, 4);
+            Console.ReadLine();
+        }
+
+        //For this tutorial, checkout https://support.microsoft.com/en-us/help/308359/how-to-write-a-simple-web-service-by-using-visual-c--net
+    }
+}
---
+++ WebServiceIntro/MathWebService/MathWebService/MathService.asmx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MathWebService
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+
+
+        ///1. I created a completely empty Active Server Page .NET (ASP.NET) application. 
+        ///2. I then created a new item within this project, thus I created a web service, hence this form you're currently viewing. 
+        ///3. Every Method (The ones you see below) that the developer wants to be used as a web service must be denoted with a [WebMethod] tag before
+        ///     the method. Otherwise the method will not be able to be used or viewed when page is running. 
+        ///     For example, if we take away the WebMethod tag from the System.Single Subtract method, that method will not be able to be used nor
+        ///     will it be able to be viewed when the page is running. 
+        ///After all of this, I created a client code application which consumes the web service in a console command window. 
+
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return (a+b);
+        }
+
+        [WebMethod]
+        public System.Single Substract(System.Single A, System.Single B)
+        {
+            return (A - B);
+        }
+
+        [WebMethod]
+        public System.Single Multiply(System.Single A, System.Single B)
+        {
+            return (A * B);
+        }
+
+        [WebMethod]
+        public System.Single Divide(System.Single A, System.Single B)
+        {
+            if (B == 0)
+                return 0;
+            return Convert.ToSingle(A / B);
+        }
+
+        //For this tutorial, checkout: https://support.microsoft.com/en-us/help/308359/how-to-write-a-simple-web-service-by-using-visual-c--net
+    }
+}
